b9a096466d29ba97b9b9320ce866d8a46dbf9f21
Pedro Vinícius <112122276+DropeVinicius@users.noreply.github.com>
2024-05-08 20:39:20 -0300

    Criando login e cadastro

---
+++ src/componentes/Auth/Cadastro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cadastro() {
+    return (
+        <div>
+            <h2>Cadastro</h2>
+            {/* Formulário de cadastro aqui */}
+        </div>
+    );
+}
+
+export default Cadastro;
