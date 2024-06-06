b288ba826ea99f787ab35dd420ea41d517d6f88d
Pedro Vinícius <112122276+DropeVinicius@users.noreply.github.com>
2024-06-06 10:17:27 -0300

    IconeHeader fix

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import perfil from '../../imagens/perfil.svg'
 // import sacola from '../../imagens/sacola.svg'
 import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
 
 const Icone = styled.li`   
     margin-right: 40px;
     width: 25px;
+    cursor: pointer; 
 `
 
 const Icones = styled.ul`
@@ -16,10 +19,19 @@ const icones = [perfil]
 
 
 function IconesHeader () {
+
+    const navigate = useNavigate();
+
+    const handleIconClick = () =>{
+        navigate('/login'); // Redireciona para a página de login
+    };
+
     return (
         <Icones>
-            { icones.map((icone) => (
-                <Icone><img src={icone}></img></Icone>
+            { icones.map((icone, index) => (
+                    <Icone key={index} onClick={handleIconClick}>
+                        <img src={icone} alt="Icone de perfil"></img>
+                    </Icone>
             ))}
         </Icones>
     )
