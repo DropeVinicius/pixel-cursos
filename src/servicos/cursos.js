9e967d9978c554a2d897ea3339530cad7c1dabde
Pedro Vinícius <112122276+DropeVinicius@users.noreply.github.com>
2024-09-24 19:31:04 -0300

    services path

--- src/servicos/cursos.js
+++ src/servicos/cursos.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const cursosAPI = axios.create({ baseURL: "https://pixel-cursos-serverv2-dd5c274eb09d.herokuapp.com/"})
+const cursosAPI = axios.create({ baseURL: "https://pixel-cursos-serverv2-dd5c274eb09d.herokuapp.com/cursos"})
 
 async function getCursos() {
     const response = await cursosAPI.get('/')
