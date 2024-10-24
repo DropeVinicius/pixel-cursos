7c75b18aa56b7ca2dc31601d86a29e3773dc62a9
Pedro Vinícius <112122276+DropeVinicius@users.noreply.github.com>
2024-10-23 21:20:19 -0300

    altera seção ultimos lançamentos

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -14,7 +14,7 @@ const UltimosLancamentosContainer = styled.section`
     align-items: center;
 `
 
-const NovosCursosContainer = styled.div `
+const NovosCursosContainer = styled.div`
     margin-top: 30px;
     margin-bottom: 30px;
     display: flex;
@@ -23,13 +23,31 @@ const NovosCursosContainer = styled.div `
     justify-content: center; /* Centraliza os itens */
     cursor: pointer;
 `
-const CursoLink = styled(NavLink) `
-    margin: 10px;
+const CursoLink = styled(NavLink)`
+  margin: 10px;
+  text-decoration: none; /* Remove o sublinhado do link */
+  color: inherit; /* Garante que a cor dos textos seja a mesma que a definida nos filhos */
+  
+  &:hover {
+    text-decoration: none; /* Garante que ao passar o mouse, o sublinhado não apareça */
+  }
 `
-const CursoImagem = styled.img `
-    border-radius: 10px;
-    width: 100%; /* Garante que a imagem ocupe toda a largura do container */
-    max-width: 300px; /* Limita o tamanho máximo da imagem */
+const CursoImagem = styled.img`
+  border-radius: 10px;
+  width: 100%; /* Garante que a imagem ocupe toda a largura do container */
+  max-width: 300px; /* Limita o tamanho máximo da imagem */
+  border: 3px solid #002F52; /* Borda sutil com transparência */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Sombra sutil para destacar mais */
+
+`
+
+const CursoTitulo = styled.p`
+  font-size: 18px;
+  color: #000;
+  text-decoration: none; /* Remove o sublinhado */
+  font-weight: bold;
+  text-align: center; /* Centraliza o texto */
+  margin-top: 10px; /* Adiciona um espaçamento superior */
 `
 
 const estiloImagem = {
@@ -39,26 +57,26 @@ const estiloImagem = {
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo 
-                cor="#EB9B00" 
-                tamanhoFonte="36px" 
+            <Titulo
+                cor="#EB9B00"
+                tamanhoFonte="36px"
             >
                 ÚLTIMOS LANÇAMENTOS
             </Titulo>
             <NovosCursosContainer>
-                {cursos.map (curso => (
-                        <CursoLink to={`/player?id=${curso.id}`} key={curso.id}>
-                            <CursoImagem src={curso.src} style={estiloImagem} alt={curso.nome}/>
-                            <p>{curso.nome}</p>
-                        </CursoLink>
+                {cursos.map(curso => (
+                    <CursoLink to={`/player?id=${curso.id}`} key={curso.id}>
+                        <CursoImagem src={curso.src} style={estiloImagem} alt={curso.nome} />
+                        <CursoTitulo>{curso.nome}</CursoTitulo>
+                    </CursoLink>
                 ))}
-            </NovosCursosContainer>     
+            </NovosCursosContainer>
             <CardRecomenda
                 titulo="Talvez você se interesse por"
                 subtitulo="Microsoft Certified: Azure Fundamentals"
                 descricao="Demonstrar conhecimento fundamental dos conceitos de nuvem e dos principais serviços do Azure, além de recursos e ferramentas de gerenciamento e governança do Azure."
                 img={imagemLivro}
-            />   
+            />
         </UltimosLancamentosContainer>
     )
 }
